579dafc91f87da467fde8ed4e5b1eb0f64552e4e
NabajyotiModak <naba93981@gmail.com>
2023-01-04 02:25:37 +0530

    added link to dynamic route pages

--- Codevolution/Routing/Catch-all-routes/catch-all/pages/product/index.js
+++ Codevolution/Routing/Catch-all-routes/catch-all/pages/product/index.js
@@ -6,6 +6,9 @@ export default function Product() {
     <div>
       <h1>product</h1>
       <Link href="/">Home</Link>
+      <Link href="/product/1">Product 1</Link>
+      <Link href="/product/2">Product 2</Link>
+      <Link href="/product/3">Product 3</Link>
     </div>
   )
 }
