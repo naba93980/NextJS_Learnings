26459d4f9db7039f8a807c5b9e7a2256bbce0d02
NabajyotiModak <naba93981@gmail.com>
2023-01-04 02:17:52 +0530

    link to /home from product page

--- Codevolution/Routing/Catch-all-routes/catch-all/pages/product/index.js
+++ Codevolution/Routing/Catch-all-routes/catch-all/pages/product/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Link from "next/link";
 
 export default function Product() {
   return (
     <div>
       <h1>product</h1>
+      <Link href="/">Home</Link>
     </div>
   )
 }
