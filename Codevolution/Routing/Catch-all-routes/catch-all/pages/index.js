24fd365ef5801cccdd4ce965b1890da84be2eafd
NabajyotiModak <naba93981@gmail.com>
2023-01-05 20:48:50 +0530

    navigation in new set up

--- Codevolution/Routing/Catch-all-routes/catch-all/pages/index.js
+++ Codevolution/Routing/Catch-all-routes/catch-all/pages/index.js
@@ -1,12 +1,9 @@
 import React from "react";
-import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <h1>Home Page</h1>
-      <Link href="/blog">Blog</Link>
-      <Link href="/product">Product</Link>
     </div>
   );
 }
--- Codevolution/Routing/Catch-all-routes/catch-all/pages/product/index.js
+++ Codevolution/Routing/Catch-all-routes/catch-all/pages/product/index.js
@@ -1,13 +1,10 @@
 import React from 'react'
-import Link from "next/link";
 
 export default function Product({productId=100}) {
 
 return (
 <div>
   <h1>product</h1>
-  <Link href="/">Home</Link>
-  <Link href={`/product/${productId}`} replace>Product {productId}</Link>
 </div>
 );
 }
