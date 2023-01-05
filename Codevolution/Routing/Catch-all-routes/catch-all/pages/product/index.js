0e8ffaf9385e7c018eea7b15b1f65ab591886ada
NabajyotiModak <naba93981@gmail.com>
2023-01-05 15:15:26 +0530

    productId received as prop

--- Codevolution/Routing/Catch-all-routes/catch-all/pages/product/index.js
+++ Codevolution/Routing/Catch-all-routes/catch-all/pages/product/index.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import Link from "next/link";
 
-export default function Product() {
-  return (
-    <div>
-      <h1>product</h1>
-      <Link href="/">Home</Link>
-      <Link href="/product/1">Product 1</Link>
-      <Link href="/product/2">Product 2</Link>
-      <Link href="/product/3">Product 3</Link>
-    </div>
-  )
+export default function Product({productId=100}) {
+
+return (
+<div>
+  <h1>product</h1>
+  <Link href="/">Home</Link>
+  <Link href={`/product/${productId}`} replace={true}>Product {productId}</Link>
+</div>
+);
 }
