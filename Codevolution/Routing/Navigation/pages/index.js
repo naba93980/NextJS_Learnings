8d36bdcec66c629cb7f61e321b486f1743c8a0f2
NabajyotiModak <naba93981@gmail.com>
2023-01-05 21:27:15 +0530

    navigating programatically using router.push

--- Codevolution/Routing/Navigation/pages/index.js
+++ Codevolution/Routing/Navigation/pages/index.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import Link from "next/link";
+import {useRouter} from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+  const handleClick = () => { 
+    console.log('Placing your order');
+    router.push('/product');
+    }
+
   return (
     <div>
       <h1>Home Page</h1>
       <Link href="/blog">Blog</Link>
       <Link href="/product">Product</Link>
+      <button onClick={handleClick}>Placing ur order</button>
     </div>
   )
 }
