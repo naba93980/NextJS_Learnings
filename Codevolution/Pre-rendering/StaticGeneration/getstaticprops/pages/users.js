e85675a66531f5cd48533d22d45d1959b7d81d2b
NabajyotiModak <naba93981@gmail.com>
2023-01-09 22:51:46 +0530

    statically generating users page that uses getStaticProps

--- Codevolution/Pre-rendering/StaticGeneration/getstaticprops/pages/users.js
+++ Codevolution/Pre-rendering/StaticGeneration/getstaticprops/pages/users.js
@@ -1,7 +1,30 @@
 import React from 'react'
 
-export default function UserList() {
+export default function UserList({users}) {
   return (
-    <div><h1>users</h1></div>
+    <div>
+      <h1>users</h1>
+      {
+        users.map((user)=>{
+          return (
+            <div key={user.id}>
+              <p>{user.id} {user.name}</p>
+              <p>{user.website}</p>
+            </div>
+          )
+        })
+      }
+      </div>
   )
+}
+
+export async function getStaticProps(){
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await response.json();
+  console.log(data);
+  return {
+    props: {
+      users: data
+    }
+  }
 }
