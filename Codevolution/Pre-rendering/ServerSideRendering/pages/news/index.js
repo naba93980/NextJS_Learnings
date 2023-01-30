4bc5bed08b93e6b51a40c848da13d89259d25044
NabajyotiModak <naba93981@gmail.com>
2023-01-30 16:55:40 +0530

    /news page that uses serverSideProps

---
+++ Codevolution/Pre-rendering/ServerSideRendering/pages/news/index.js
@@ -0,0 +1,24 @@
+export default function NewsArticleList({ articles }) {
+    return (
+        <>
+            <h1>List of Articles</h1>
+            {articles.map((article) => {
+                return (
+                    <div key={article.id}>
+                        <h2>{article.id} {article.title} | {article.category}</h2>
+                    </div>
+                )
+            })}
+        </>
+    )
+}
+
+export async function getServerSideProps() {
+    const response = await fetch('http://localhost:4000/news')
+    const data = await response.json();
+    return {
+        props: {
+            articles: data,
+        }
+    }
+}
