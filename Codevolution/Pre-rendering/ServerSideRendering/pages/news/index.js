ebec99693bcfcae33d496c4e8a6391e175757f72
NabajyotiModak <naba93981@gmail.com>
2023-01-30 21:23:00 +0530

    additional console.log inside getServerSide props

--- Codevolution/Pre-rendering/ServerSideRendering/pages/news/index.js
+++ Codevolution/Pre-rendering/ServerSideRendering/pages/news/index.js
@@ -16,6 +16,7 @@ export default function NewsArticleList({ articles }) {
 export async function getServerSideProps() {
     const response = await fetch('http://localhost:4000/news')
     const data = await response.json();
+    console.log("pre-rendering news articles");
     return {
         props: {
             articles: data,
