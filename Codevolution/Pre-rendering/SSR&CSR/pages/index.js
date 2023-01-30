9b8b12a6a2004f6dca6f0368be577b52b7b51b01
NabajyotiModak <naba93981@gmail.com>
2023-01-31 03:31:46 +0530

    setup

---
+++ Codevolution/Pre-rendering/SSR&CSR/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>NextJS pre-rendering</h1>
+      <Link href="/users">Users</Link>
+      </div>
+   
+  )
+}
