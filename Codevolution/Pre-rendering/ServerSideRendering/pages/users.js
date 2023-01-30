f5c95dd20b1c1a7ed4593142e8e7cbeb7763ec1e
NabajyotiModak <naba93981@gmail.com>
2023-01-30 16:55:16 +0530

    basic setup

---
+++ Codevolution/Pre-rendering/ServerSideRendering/pages/users.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import User from '../components/user'
+
+export default function UserList({users}) {
+  return (
+    <div>
+      <h1>users</h1>
+      {
+        users.map((user)=>{
+          return (
+            <div key={user.id}>
+              <User user={user}/>
+            </div>
+          )
+        })
+      }
+      </div>
+  )
+}
+
+export async function getStaticProps(){
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await response.json();
+  return {
+    props: {
+      users: data
+    }
+  }
+}
