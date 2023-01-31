83e30026843ec200e09e219e4fd3abaf36a75f7f
NabajyotiModak <naba93981@gmail.com>
2023-01-31 05:50:15 +0530

    added code to fetch data based on query params inside getServerProps

--- Codevolution/Pre-rendering/SSR_CSR/pages/events.js
+++ Codevolution/Pre-rendering/SSR_CSR/pages/events.js
@@ -1,14 +1,17 @@
 import { useState } from "react";
+import { useRouter } from "next/router";
 
 function EventList({ eventlList }) {
-    const [events, setEvents]=useState(eventlList);
+    const [events, setEvents] = useState(eventlList);
+    const router = useRouter();
 
     const fetchSportsEvents = async () => {
         const response = await fetch('http://localhost:4000/events?category=sports');
         const data = await response.json();
         setEvents(data);
+        router.push('/events?category=sports', undefined, { shallow: true });
     }
-    
+
     return (
         <>
             <h1>List of events</h1>
@@ -31,9 +34,12 @@ function EventList({ eventlList }) {
 
 export default EventList
 
-export async function getServerSideProps() {
+export async function getServerSideProps(context) {
     console.log("fetching events");
-    const response = await fetch('http://localhost:4000/events')
+    const { query } = context;
+    const { category } = query;
+    const queryString = category ? '?category=sports' : '';
+    const response = await fetch(`http://localhost:4000/events${queryString}`);
     const data = await response.json()
     return {
         props: {
