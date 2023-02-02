f68305190a4e0e3d8d0f44d6e9bff57c4cc8ca4d
NabajyotiModak <naba93981@gmail.com>
2023-02-02 23:37:49 +0530

    added comment to submit comment

--- Codevolution/API_Routes/next-api/pages/comments/index.js
+++ Codevolution/API_Routes/next-api/pages/comments/index.js
@@ -2,16 +2,32 @@ import React, { useState } from 'react'
 
 export default function CommentsPage() {
     const [comments, setComments] = useState(null);
+    const [comment, setComment] = useState('');
 
     const fetchComments = async () => {
         const response = await fetch('/api/comments');
         const data = await response.json();
         setComments(data);
     }
+
+    const submitComment = async () => {
+        const response = await fetch('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({ comment }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        const data = await response.json();
+        console.log(data);
+    }
+
     return (
         <div>
+            <input type="text" value={comment} onChange={(e) => setComment(e.target.value)} />
+            <button onClick={submitComment}>Submit comment</button>
             <button onClick={fetchComments}>Load Comments</button>
-            {comments&&comments.map((comment)=>{
+            {comments && comments.map((comment) => {
                 return (
                     <div key={comment.id}>
                         {comment.id}  {comment.text}
