37e7ab213b1a6486666c258b119732c6f4788067
NabajyotiModak <naba93981@gmail.com>
2023-02-03 23:52:10 +0530

    component lvl styling

---
+++ Codevolution/Styling/next-styles/pages/contact.js
@@ -0,0 +1,10 @@
+import styles from '../styles/Contact.module.css'
+
+export default function Contact(){
+    return (
+        <>
+        <h2 className={styles.highlight}>Contact Page</h2>
+        <button className="btn btn-primary">Primary</button>
+        </>
+    )
+}
