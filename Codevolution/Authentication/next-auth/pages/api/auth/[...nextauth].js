f6c403f60b05c45fc76b85eb9a56d9595dc6b2ed
NabajyotiModak <naba93981@gmail.com>
2023-02-10 02:31:52 +0530

    authentication providers page

---
+++ Codevolution/Authentication/next-auth/pages/api/auth/[...nextauth].js
@@ -0,0 +1,12 @@
+import  GithubProvider  from 'next-auth/providers/github'
+import NextAuth from 'next-auth'
+
+export default NextAuth({
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+        }),
+    ],
+})
+
