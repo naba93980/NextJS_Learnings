3ac64644f214cbce1f32198e9bc2cc397854397e
NabajyotiModak <naba93981@gmail.com>
2023-02-11 00:18:19 +0530

    uses getSession() to check for session and thn render the page

--- Codevolution/Authentication/next-server-side-auth/pages/blog.js
+++ Codevolution/Authentication/next-server-side-auth/pages/blog.js
@@ -1,8 +1,18 @@
-export default function Blog() {
-    return (
-      <div>
-        <h1>Blog Page</h1>
-      </div>
-    )
+import { getSession } from 'next-auth/react'
+
+function Blog({ data }) {
+  return <h1>Blog page - {data}</h1>
+}
+
+export default Blog
+
+export async function getServerSideProps(context) {
+  console.log("inside server side props")
+  const session = await getSession(context)
+  return {
+    props: {
+      data: session ? 'List of 100 perzonalized blogs' : 'List of free blogs',
+      session                    // helps by sending session obj to pageProps in _app.js which then can be Provided to Navbar component serverside using useSession()
+    }
   }
-  
+}
