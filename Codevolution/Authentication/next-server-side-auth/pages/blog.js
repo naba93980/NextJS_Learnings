e5106a958199ea5b6f1f701d2cc94fb4cecced16
NabajyotiModak <naba93981@gmail.com>
2023-02-11 00:55:58 +0530

    serverside redirecting if session is not found

--- Codevolution/Authentication/next-server-side-auth/pages/blog.js
+++ Codevolution/Authentication/next-server-side-auth/pages/blog.js
@@ -9,6 +9,15 @@ export default Blog
 export async function getServerSideProps(context) {
   console.log("inside server side props")
   const session = await getSession(context)
+
+  if (!session)
+    return {
+      redirect: {
+        destination: '/api/auth/signin?callbackUrl=http://localhost:3000/blog',
+        permanent: false,
+      }
+    }
+
   return {
     props: {
       data: session ? 'List of 100 perzonalized blogs' : 'List of free blogs',
